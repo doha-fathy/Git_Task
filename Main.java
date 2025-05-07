a0bea1872accbd988cf28f9d7508198074d2a99f
Doha Fathy <fathydoha234@gmail.com>
2025-05-07 23:36:18 +0300

    ID 20230197

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Doha Fathy");
+        System.out.println("Doha Fathy ");
         System.out.println("20230197");
 
 
